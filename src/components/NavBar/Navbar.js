8a62afd3b67b91ac79e3b824a1866a5fe4358c4f
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-10-03 13:58:37 -0300

    atualizando o método Light Mode e Dark mode da página
    - Utilizando localStorage para armazenar o Tema escolhido

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,37 +1,8 @@
 import './Navbar.css'
 import {MdDarkMode, MdLightMode, MdDensityMedium, MdClear} from 'react-icons/md'
 import Logo from './../../icons/favicon.ico'
-var Mode = '#light'
 
 function NavBar(){
-
-    Mode = window.location.hash
-    async function DedetectMode(){
-        var DarkModeBtn = document.getElementById('DarkMode')
-        var lightModeBtn = document.getElementById('LightMode')
-        var DarkModeBtnMobile = document.getElementById('DarkModeMobile')
-        var lightModeBtnMobile = document.getElementById('LightModeMobile')
-        var htmlBody = document.getElementById('html')
-        if(Mode === '#dark'){
-            htmlBody.removeAttribute('class')
-            htmlBody.setAttribute('class', 'dark-mode')
-            DarkModeBtn.style.display = 'flex'
-            lightModeBtn.style.display = 'none'
-            DarkModeBtnMobile.style.display = 'flex'
-            lightModeBtnMobile.style.display = 'none'
-            Mode = '#dark'
-        }else{
-            htmlBody.removeAttribute('class')
-            htmlBody.setAttribute('class', 'light-mode')
-            DarkModeBtn.style.display = 'none'
-            DarkModeBtnMobile.style.display = 'none'
-            lightModeBtn.style.display = 'flex'
-            lightModeBtnMobile.style.display = 'flex'
-            Mode = '#light'
-        }
-    }
-   setTimeout( DedetectMode, 100)
-
 
     function OpenNavbarMobile(el) {
         var Open = document.getElementById('NavbarMobileOpen')
@@ -52,35 +23,30 @@ function NavBar(){
         }
     }
     function ModeScreen(){
+        var Tema = window.localStorage.getItem('Tema')
         var DarkModeBtn = document.getElementById('DarkMode')
         var lightModeBtn = document.getElementById('LightMode')
         var DarkModeBtnMobile = document.getElementById('DarkModeMobile')
         var lightModeBtnMobile = document.getElementById('LightModeMobile')
-        if(Mode === '#light'){
+        if(Tema === 'Light'){
             DarkModeBtn.style.display = 'flex'
             lightModeBtn.style.display = 'none'
             DarkModeBtnMobile.style.display = 'flex'
             lightModeBtnMobile.style.display = 'none'
-            Mode = '#dark'
-            window.location.hash = Mode
-            DedetectMode()
+            window.localStorage.setItem('Tema', 'Dark')
+            window.location.reload()
         }else {
             DarkModeBtn.style.display = 'none'
             DarkModeBtnMobile.style.display = 'none'
             lightModeBtn.style.display = 'flex'
             lightModeBtnMobile.style.display = 'flex'
-            Mode = '#light'
-            window.location.hash = Mode
-            DedetectMode()
+            window.localStorage.setItem('Tema', 'Light')
+            window.location.reload()
         }
         
     }
     function PageRedirect(value){
-        if(Mode === 'light'){
-            window.location = `${value}`            
-        }else{
-            window.location = `${value}${Mode}`
-        }
+        window.location = `${value}`            
     }
 
     return(
@@ -93,7 +59,7 @@ function NavBar(){
                 <button onClick={()=>PageRedirect('/inicio')} ><b>Inicio</b></button>
                 <button onClick={()=>PageRedirect('/sobre')}><b>Sobre</b></button>
                 <button onClick={()=>PageRedirect('/projetos')}><b>Projetos</b></button>
-                <button onClick={()=>PageRedirect('/sobre')}><b>Contato</b></button>
+                <button onClick={()=>PageRedirect('/contato')}><b>Contato</b></button>
                 <button onClick={()=> ModeScreen()}><span id='DarkMode'><MdDarkMode size={20}/></span><span id='LightMode'><MdLightMode size={20}/></span></button>
             </div>
             <div className='NavbarHref Mobile'>
@@ -102,7 +68,7 @@ function NavBar(){
                     <button  className='BtnMobileNavbar' onClick={()=>PageRedirect('/inicio')}><b>Inicio</b></button>
                     <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/sobre')}><b>Sobre</b></button>
                     <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/projetos')}><b>Projetos</b></button>
-                    <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/sobre')}><b>Contato</b></button>
+                    <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/contato')}><b>Contato</b></button>
                     <button className='BtnMobileNavbar' onClick={()=> ModeScreen()}><span id='DarkModeMobile'><MdDarkMode size={20}/></span><span id='LightModeMobile'><MdLightMode size={20}/></span></button>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -9,24 +9,59 @@ import NavBar from './components/NavBar/Navbar';
 import SearchProjeto from './Pages/ProjetosPage/Search/Search';
 import ProjetosPage from './Pages/ProjetosPage/Projetos.js'; 
  
-window.hash = 'dark'
-// console.log(window.hash)
-// console.log(window)
+/* 
+console.log(window.localStorage.setItem('Tema', 'Light'))
+console.log(window.localStorage.getItem('Tema'))
+*/
+var Tema = window.localStorage.getItem('Tema')
+
+if(Tema === null){
+  window.localStorage.setItem('Tema', 'Light')
+}else{
+  console.log('Temos um tema ativo!')
+}
+
+async function DedetectMode(){
+  var DarkModeBtn = document.getElementById('DarkMode')
+  var lightModeBtn = document.getElementById('LightMode')
+  var DarkModeBtnMobile = document.getElementById('DarkModeMobile')
+  var lightModeBtnMobile = document.getElementById('LightModeMobile')
+  var htmlBody = document.getElementById('html')
+    if(Tema === 'Dark'){
+        htmlBody.removeAttribute('class')
+        htmlBody.setAttribute('class', 'dark-mode')
+        DarkModeBtn.style.display = 'flex'
+        lightModeBtn.style.display = 'none'
+        DarkModeBtnMobile.style.display = 'flex'
+        lightModeBtnMobile.style.display = 'none'
+        window.localStorage.setItem('Tema', 'Dark')
+        console.log(Tema)
+  }else{
+        htmlBody.removeAttribute('class')
+        htmlBody.setAttribute('class', 'light-mode')
+        DarkModeBtn.style.display = 'none'
+        DarkModeBtnMobile.style.display = 'none'
+        lightModeBtn.style.display = 'flex'
+        lightModeBtnMobile.style.display = 'flex'
+        window.localStorage.setItem('Tema', 'Light')
+        console.log(Tema)
+  }
+}
+setTimeout( DedetectMode, 100)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-   <NavBar/>
-   <Background/>
+root.render( 
+  <BrowserRouter> 
+   <NavBar/> 
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/inicio" element={<Inicio />} />
       <Route path="/sobre" element={<Sobre />} />
-      <Route path='/contato #contato' element={<Sobre/>}/>
+      <Route path='/contato' element={<Sobre/>}/>
       <Route path='/projetos' element={<ProjetosPage/>}/>
       <Route path='/projetos/search' element={<SearchProjeto/>}/>
-
     </Routes>
-  </BrowserRouter>
+   <Background/>
+  </BrowserRouter> 
 );
 
