050376374c3245e542840d69f27e2edcbbe3aa01
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-10-12 02:14:25 -0300

    Atualizando o /Sobre
    -Adicionando novas tecnologias para a lista e atualização do título.
    
    - Adicionando um destaque para as tecnologias mais utilizadas.

--- src/Pages/Sobre/components/habilidades.js
+++ src/Pages/Sobre/components/habilidades.js
@@ -3,46 +3,62 @@ import {FaCode} from 'react-icons/fa6'
 
 
 const PHP = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg'
-const Html = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
-const Javascript = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
-const Css = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
-const Nodejs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
-const MySQL = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
-const Python = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
-const Reactjs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+        const Html = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+        const Javascript = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+        const Css = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+        const Nodejs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+        const MySQL = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+        const Python = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
+        const Reactjs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+        const Firebase = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg'
+        const Typescript = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg'
+        const Npm = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg'
+        const Discordjs = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/discordjs/discordjs-original.svg'
 
 
 function Habilidades(){
     return (
         <div>
             <div className="TitleHabi">
-                <h3><FaCode/>Linguagens</h3>
+                <h3><FaCode/>Tecnologias</h3>
             </div>
             <div className="TypHabi">
-                <div className="languagesHabi">
-                    <p>Html</p>
-                    <img alt="Html" src={Html} />
-                </div>
-                <div className="languagesHabi">
-                    <p>Css</p>
-                    <img alt="Css" src={Css} />
-                </div>
-                <div className="languagesHabi">
-                    <p>Javascript</p>
-                    <img alt="Javascript" src={Javascript} />
-                </div>
-                <div className="languagesHabi">
-                    <p>React.js</p>
-                    <img alt="Node.js" src={Reactjs} />
-                </div>
-                <div className="languagesHabi">
-                    <p>Node.js</p>
-                    <img alt="Node.js" src={Nodejs} />
-                </div>
-                <div className="languagesHabi">
-                    <p>MySQL</p>
-                    <img alt="MySQL" src={MySQL} />
-                </div>
+                <fieldset className="HabiMaisUtilits">
+                    <legend>Mais utilizadas</legend>
+                    <div className="languagesHabi">
+                        <p>Html</p>
+                        <img alt="Html" src={Html} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>Css</p>
+                        <img alt="Css" src={Css} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>Javascript</p>
+                        <img alt="Javascript" src={Javascript} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>React.js</p>
+                        <img alt="React.js" src={Reactjs} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>Node.js</p>
+                        <img alt="Node.js" src={Nodejs} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>MySQL</p>
+                        <img alt="MySQL" src={MySQL} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>Npm </p>
+                        <img alt="Npm" src={Npm} />
+                    </div>
+                    <div className="languagesHabi">
+                        <p>Firebase</p>
+                        <img alt="Firebase" src={Firebase} />
+                    </div>
+                    <span><a href="/projetos">Meus Projetos!</a></span>
+                </fieldset>
                 <div className="languagesHabi">
                     <p>PHP</p>
                     <img alt="PHP" src={PHP} />
@@ -50,6 +66,14 @@ function Habilidades(){
                 <div className="languagesHabi">
                     <p>Python</p>
                     <img alt="Python" src={Python} />
+                </div>
+                <div className="languagesHabi">
+                    <p>Discord.js</p>
+                    <img alt="Discordjs" src={Discordjs} />
+                </div>
+                <div className="languagesHabi">
+                    <p>Typescript</p>
+                    <img alt="Typescript" src={Typescript} />
                 </div>
             </div>
         </div>
