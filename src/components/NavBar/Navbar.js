5a2820e78e6c16f7fdd01ac50bd9cc8d6b6c2832
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-09-19 13:33:48 -0300

    Recriando a base do html e css

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,85 +1,21 @@
 import './Navbar.css'
-import {MdDarkMode, MdLightMode, MdDensityMedium, MdClear} from 'react-icons/md'
-import Logo from './../../icons/favicon.ico'
+import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
 
 function NavBar(){
 
-    function OpenNavbarMobile(el) {
-        var Open = document.getElementById('NavbarMobileOpen')
-        var Close = document.getElementById('NavbarMobileClose')
-        var display = document.getElementById(el).style.display;
-        if(display === "none"){
-            Open.style.display = 'none'
-            Close.style.display = 'flex'
-            document.getElementById(el).style.display = 'flex';
-        }else if(display === 'flex'){
-            Open.style.display = 'flex'
-            Close.style.display = 'none'
-            document.getElementById(el).style.display = 'none';
-        }else{
-            Open.style.display = 'none'
-            Close.style.display = 'flex'
-            document.getElementById(el).style.display = 'flex';
-        }
-    }
-    function ModeScreen(){
-        var Tema = window.localStorage.getItem('Tema')
-        var DarkModeBtn = document.getElementById('DarkMode')
-        var lightModeBtn = document.getElementById('LightMode')
-        var DarkModeBtnMobile = document.getElementById('DarkModeMobile')
-        var lightModeBtnMobile = document.getElementById('LightModeMobile')
-        var htmlBody = document.getElementById('html')
-        if(Tema === 'light'){
-            htmlBody.removeAttribute('class')
-            htmlBody.setAttribute('class', 'dark-mode')
-            DarkModeBtn.style.display = 'flex'
-            lightModeBtn.style.display = 'none'
-            DarkModeBtnMobile.style.display = 'flex'
-            lightModeBtnMobile.style.display = 'none'
-            window.localStorage.setItem('Tema', 'dark')
-        }else {
-            htmlBody.removeAttribute('class')
-            htmlBody.setAttribute('class', 'light-mode')
-            DarkModeBtn.style.display = 'none'
-            DarkModeBtnMobile.style.display = 'none'
-            lightModeBtn.style.display = 'flex'
-            lightModeBtnMobile.style.display = 'flex'
-            window.localStorage.setItem('Tema', 'light')
-        }
-        
-    }
-    function PageRedirect(value){
-        window.location = `${value}`            
-    }
-
     return(
-        <>
-            <div className='Navbar'>
-                <div className='NavbarLogo'>
-                        <img alt='tes' src={Logo}/> 
-                        <a href='https://www.instagram.com/rafaelparroni/'><b>Rafael Paroni Silva</b></a>
-                </div>
-                <div className='NavbarHref '>
-                    <button onClick={()=>PageRedirect('/inicio')} ><b>Inicio</b></button>
-                    <button onClick={()=>PageRedirect('/sobre')}><b>Sobre</b></button>
-                    <button onClick={()=>PageRedirect('/projetos')}><b>Projetos</b></button>
-                    <button onClick={()=>PageRedirect('/curriculo')}><b>Curriculo</b></button>
-                    <button onClick={()=>PageRedirect('/contato')}><b>Contato</b></button>
-                    <button onClick={()=> ModeScreen()}><span id='DarkMode'><MdDarkMode size={20}/></span><span id='LightMode'><MdLightMode size={20}/></span></button>
-                </div>
-                <div className='NavbarHref Mobile'>
-                    <button  onClick={()=> OpenNavbarMobile('NavbarMobile')}><MdDensityMedium id='NavbarMobileOpen' size={25}/> <MdClear id='NavbarMobileClose' size={25}/></button>
-                    <div id='NavbarMobile'  className='ScreenMobileNavbar'>
-                        <button  className='BtnMobileNavbar' onClick={()=>PageRedirect('/inicio')}><b>Inicio</b></button>
-                        <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/sobre')}><b>Sobre</b></button>
-                        <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/projetos')}><b>Projetos</b></button>
-                        <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/curriculo')}><b>Curriculo</b></button>
-                        <button className='BtnMobileNavbar' onClick={()=>PageRedirect('/contato')}><b>Contato</b></button>
-                        <button className='BtnMobileNavbar' onClick={()=> ModeScreen()}><span id='DarkModeMobile'><MdDarkMode size={20}/></span><span id='LightModeMobile'><MdLightMode size={20}/></span></button>
-                    </div>
-                </div>
+        <div className='navbar'>
+            <div className='menu'>
+                <button>Inicio</button>
+                <button>Sobre</button>
+                <button>Projetos</button>
             </div>
-        </>
+            <div className='redes'>
+                <button><BsLinkedin/></button>
+                <button><BsGithub/></button>
+                <button><BsInstagram/></button>
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -10,62 +10,21 @@
   // Import Pages/Componentes
   import Inicio from './Pages/Inicio/Inicio';
   import Sobre from './Pages/Sobre/Sobre';
-  import Background from './components/Background/Background';
-  import NavBar from './components/NavBar/Navbar';
-  import ScrollBtn from './components/ButtonScroll/Scroll';
   import SearchProjeto from './Pages/ProjetosPage/Search/Search';
   import ProjetosPage from './Pages/ProjetosPage/Projetos.js'; 
   import Curriculo from './Pages/curriculo/Curriculo.js';
+
+
+
+  // Import Contents
+  import NavBar from './components/NavBar/Navbar.js';
   
-  var Tema = window.localStorage.getItem('Tema')
 
-  if(Tema === null){
-    window.localStorage.setItem('Tema', 'light')
-  }else{
-    console.log('Temos um tema ativo!')
-  }
-
-  async function DedetectMode(){
-    var DarkModeBtn = document.getElementById('DarkMode')
-    var lightModeBtn = document.getElementById('LightMode')
-    var DarkModeBtnMobile = document.getElementById('DarkModeMobile')
-    var lightModeBtnMobile = document.getElementById('LightModeMobile')
-    var htmlBody = document.getElementById('html')
-      if(Tema === 'dark'){
-          htmlBody.setAttribute('class', 'dark-mode')
-          DarkModeBtn.style.display = 'flex'
-          lightModeBtn.style.display = 'none'
-          DarkModeBtnMobile.style.display = 'flex'
-          lightModeBtnMobile.style.display = 'none'
-          window.localStorage.setItem('Tema', 'dark')
-          console.log(Tema)
-    }else{
-          htmlBody.setAttribute('class', 'light-mode')
-          DarkModeBtn.style.display = 'none'
-          DarkModeBtnMobile.style.display = 'none'
-          lightModeBtn.style.display = 'flex'
-          lightModeBtnMobile.style.display = 'flex'
-          window.localStorage.setItem('Tema', 'light')
-          console.log(Tema)
-    }
-  }
-  setTimeout( DedetectMode, 100)
-
-
-  window.addEventListener("scroll", (event) => {
-    var scrollBtn = document.getElementById('BtnScroll')
-    if(window.scrollY > 281){
-      scrollBtn.style.display = 'flex'
-    }else{
-      scrollBtn.style.display = 'none'
-    }
-  });
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render( 
     <BrowserRouter> 
-    <NavBar/> 
-    <ScrollBtn/>
+     <NavBar/>
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/inicio" element={<Inicio />} />
@@ -75,7 +34,6 @@
         <Route path='/projetos/search' element={<SearchProjeto/>}/>
         <Route path='/curriculo' element={<Curriculo/>}/>
       </Routes>
-    <Background/>  
     </BrowserRouter> 
   );
 
