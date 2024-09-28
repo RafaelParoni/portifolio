dc66391885814d3431e9139aab9a37663df0c386
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-09-28 17:19:25 -0300

    Finalizando a função de Tema
    Edição de Tema
    Detector de Tema

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,12 +1,27 @@
 import './Navbar.css'
-import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
-import { ModeDetection } from '../../functions/ThemeMode';
+import { BsInstagram, BsGithub, BsLinkedin, BsMoonFill, BsFillBrightnessLowFill    } from "react-icons/bs";
+import { AutoModeDetection, SetNewMode } from '../../functions/ThemeMode';
 
 
 function NavBar(){
 
-    function alternMode(value){
-        
+    function alternMode(){
+        var themeBtn = document.getElementById("ThemeBtn")
+        if(AutoModeDetection() === 'Dark'){
+            themeBtn.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8.5 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707zM3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z"></path></svg>'
+            SetNewMode('Light')
+        }else{
+            themeBtn.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path></svg>'
+            SetNewMode('Dark')
+        }
+    }
+
+    function ThemeBtn(){
+        if(AutoModeDetection() === 'Dark'){
+            return <span><BsMoonFill/></span>
+        }else{
+            return <span><BsFillBrightnessLowFill/></span>
+        }
     }
 
     return(
@@ -20,9 +35,8 @@ function NavBar(){
                 <button><BsLinkedin/></button>
                 <button><BsGithub/></button>
                 <button><BsInstagram/></button>
-                <button onClick={() => alternMode()} >
-                    <span id='LightBtn' style={{display: 'auto'}}>A</span>
-                    <span id='DarkBtn' style={{display: 'none'}}>B</span>
+                <button id='ThemeBtn' onClick={() => alternMode()} >
+                    <ThemeBtn />
                 </button>
             </div>
         </div>
--- src/functions/ThemeMode.js
+++ src/functions/ThemeMode.js
@@ -1,30 +1,37 @@
 
 var Theme = 'Light'
 var id = document.getElementById("html")
-var log = document.getElementById("log")
 
 function AutoModeDetection(){
-
     if(window.localStorage.getItem("Theme") === ' ' || window.localStorage.getItem("Theme") === undefined || window.localStorage.getItem("Theme") === null){
-       
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             // dark mode
-
             window.localStorage.setItem("Theme", 'Dark')
+            id.setAttribute('class', 'Dark')
             Theme = 'Dark'
         }else{
             // Light Mode
-
             window.localStorage.setItem("Theme", 'Light')
+            id.setAttribute('class', 'Light')
             Theme = 'Light'
         }
-
         return Theme
     }else{  
         Theme = window.localStorage.getItem('Theme')
+        id.setAttribute('class', Theme)
         return Theme
     }
 }
 
+function SetNewMode(Mode){
+    id.setAttribute('class', Mode)
+    if(Mode === 'Dark' || Mode === 'Light'){
+        id.setAttribute('class', Mode)
+        window.localStorage.setItem("Theme", Mode)
+    }else{
+        console.log("Algo errado: " + Mode)
+    }
+}
 
-export {AutoModeDetection, Theme}
+
+export {AutoModeDetection, Theme, SetNewMode}
