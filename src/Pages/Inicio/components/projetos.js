400c2a13d3c9ee9d21394eb27222c02ad5acca30
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-11-02 00:47:07 -0300

    Adicionando Todos os repositorios do github na pagina com a API

--- src/Pages/Inicio/components/projetos.js
+++ src/Pages/Inicio/components/projetos.js
@@ -1,7 +1,61 @@
+import { useState } from "react"
+import axios from "axios"
 
+import { BsGithub, BsBrowserChrome, BsStarFill, BsCupFill   } from "react-icons/bs";
 
 function Projetos(){
 
+    const [List, setList] = useState([])
+
+    async function SearchRepositorios(){
+        try {
+            const options = {
+            method: 'GET',
+            url: 'https://api.github.com/users/RafaelParoni/repos' // url: `https://api.github.com/repos/RafaelParoni/${NameProjeto}` // 'https://api.github.com/users/RafaelParoni/repos'
+            } 
+            const results = await axios.request(options)
+            var TecsList = results.data
+    
+            setList(TecsList)
+        }catch{
+            setList(['Not found'])
+        }
+    }
+
+    function setReposTecList(){
+        if(List.length < 1){
+            SearchRepositorios()
+        }
+    }
+    setReposTecList()
+
+    function GenerateListResp({item, index}){
+        console.log(item)
+        var description = item.description
+        try{
+            if(description.length > 25){
+                description = description.slice(0, 70)
+                description = description + '...'
+            }
+        }catch{
+            description = ''
+        }
+        var data = item.updated_at
+        data = data.slice(0,10)
+        return (
+            <div key={index} className="rep">
+                <h2 style={{margin: '0px'}}>{item.name}</h2>
+                <span>{description}</span>
+                <span style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><BsCupFill /> Links:</span>
+                <div className="links">
+                    <a href={item.html_url} target="noreferrer"><BsGithub/></a>
+                    <a href={item.homepage} target="noreferrer"><BsBrowserChrome/></a>
+                </div>
+                <span> <BsStarFill color="#ffff00"/>: {item.stargazers_count} -  </span>
+                <span className="last-updata"> Ultima atualização: {data} </span> 
+            </div>
+        )
+    }
 
     return (
         <div className="projetos">
@@ -9,21 +63,8 @@ function Projetos(){
 
             </div>
             <div className="repositorios">
-                <div className="rep">
-
-                </div>
-                <div className="rep">
-
-                </div>
-                <div className="rep">
-
-                </div>
-                <div className="rep">
-
-                </div>
-                <div className="rep">
-
-                </div>
+                <h3 style={{textAlign: 'center'}}>Projetos no Github <BsGithub/></h3>
+                {List.map((List, index) => <GenerateListResp item={List} index={index} />)}
             </div>
         </div>
     )
