33b7d70acd705caa40d9edb6d0e6b790474b963a
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2024-11-02 03:34:40 -0300

    Add projeto destaque
    adicionando função onde pega o projeto destaque, retira ele da lista de projetos no Github e salva os dados dele no "ProjetoDestaque"

--- src/Pages/Inicio/components/projetos.js
+++ src/Pages/Inicio/components/projetos.js
@@ -6,6 +6,7 @@ import { BsGithub, BsBrowserChrome, BsStarFill, BsCupFill   } from "react-icons/
 function Projetos(){
 
     const [List, setList] = useState([])
+    const [ProjetoDestaque, setProjetoDestaque] = useState([])
 
     async function SearchRepositorios(){
         try {
@@ -17,6 +18,17 @@ function Projetos(){
             var TecsList = results.data
     
             setList(TecsList)
+            var i = 0
+            for(i in results.data){
+                if(results.data[i].topics.length > 0){
+                    var x = 0
+                    for(x in results.data[i].topics){
+                        if(results.data[i].topics[x] === 'destaques'){
+                            setProjetoDestaque(results.data[i])
+                        }
+                    }
+                }
+            }
         }catch{
             setList(['Not found'])
         }
@@ -29,37 +41,54 @@ function Projetos(){
     }
     setReposTecList()
 
-    function GenerateListResp({item, index}){
-        console.log(item)
-        var description = item.description
-        try{
-            if(description.length > 25){
-                description = description.slice(0, 70)
-                description = description + '...'
+    function GenerateListResp({item, index}){ 
+        if(ProjetoDestaque.id === item.id){
+            return (<></>)
+        }else{
+            var description = item.description
+            try{
+                if(description.length > 25){
+                    description = description.slice(0, 70)
+                    description = description + '...'
+                }
+            }catch{
+                description = ''
             }
-        }catch{
-            description = ''
-        }
-        var data = item.updated_at
-        data = data.slice(0,10)
-        return (
-            <div key={index} className="rep">
-                <h2 style={{margin: '0px'}}>{item.name}</h2>
-                <span>{description}</span>
-                <span style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><BsCupFill /> Links:</span>
-                <div className="links">
-                    <a href={item.html_url} target="noreferrer"><BsGithub/></a>
-                    <a href={item.homepage} target="noreferrer"><BsBrowserChrome/></a>
+            var data = item.updated_at
+            try {
+                data = data.slice(0,10)
+            }catch{
+                data = 'dataError :c'
+            }
+            return (
+                <div key={index} className="rep">
+                    <h2 style={{margin: '0px'}}>{item.name}</h2>
+                    <span>{description}</span>
+                    <span style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><BsCupFill /> Links:</span>
+                    <div className="links">
+                        <a href={item.html_url} target="noreferrer"><BsGithub/></a>
+                        <a href={item.homepage} target="noreferrer"><BsBrowserChrome/></a>
+                    </div>
+                    <span> <BsStarFill color="#ffff00"/>: {item.stargazers_count} -  </span>
+                    <span className="last-updata"> Ultima atualização: {data} </span> 
                 </div>
-                <span> <BsStarFill color="#ffff00"/>: {item.stargazers_count} -  </span>
-                <span className="last-updata"> Ultima atualização: {data} </span> 
-            </div>
-        )
+            )
+        }
+        
     }
 
     return (
         <div className="projetos">
             <div className="desq">
+                {Object.keys(ProjetoDestaque).length === 0 && (
+                    <h1>NENHUM PROJETO EM DESTAQUE</h1>
+                )}
+                {Object.keys(ProjetoDestaque).length > 0 && (
+                    <div>
+                        PROJETO EM DESTAQUE
+                    </div>
+
+                )}
 
             </div>
             <div className="repositorios">
